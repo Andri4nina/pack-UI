e68b4263ae7bc7c6b75a5766eb35d5ffc1fd9628
Andri4nina <raso4m4ndrianina@gmail.com>
2024-04-13 21:21:52 +0300

    pack win8

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar')],
 }
